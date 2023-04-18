2142787f7f4ddb822e1256ab3165779538ae5b82
Jeon Minji <mjjeon2645@gmail.com>
2023-04-18 17:01:39 +0900

    Todo page UI 구현
    Todo page UI 구현
    - 입력필드 및 추가 버튼은 TodoInput 컴포넌트
    - 1개의 Todo는 반복되는 형태로 공용 컴포넌트로 사용 가능해보여 common 폴더에 구현
    - 해당 Todo들을 모두 표현하는 TodoLists 컴포넌트

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 
 import SigninPage from './pages/SigninPage';
 import SignupPage from './pages/SignupPage';
+import TodoPage from './pages/TodoPage';
 
 export default function App() {
   return (
@@ -9,6 +10,7 @@ export default function App() {
       <Routes>
         <Route path="/signin" element={<SigninPage />} />
         <Route path="/signup" element={<SignupPage />} />
+        <Route path="/todo" element={<TodoPage />} />
       </Routes>
     </div>
   );
---
+++ src/pages/TodoPage.jsx
@@ -0,0 +1,11 @@
+import TodoInput from '../components/TodoInput';
+import TodoLists from '../components/TodoLists';
+
+export default function TodoPage() {
+  return (
+    <>
+      <TodoInput />
+      <TodoLists />
+    </>
+  );
+}
